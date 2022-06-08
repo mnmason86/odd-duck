5e59ed581411648ee9eaa3ec3ec450e87b12eb56
Mandy Mason <mnmason86@gmail.com>
2022-06-08 00:48:44 -0400

    set up initial html structure, added //objective notes to app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+'use-strict';
+
+// Create a constructor function that creates an object associated with each product and has these properties:
+//    Product name
+//    File path of img
+//    Number of times img has been shown
+//    Number of times image has been clicked
+
+
+
+
+// Create an algorithm that randomly generates 3 unique product images from the img directory and display them side-by-side in the browser window
+
+
+
+// Attach an Event Listener to the section of the HTML page where images are displayed.
+
+
+
+// Generate 3 new random products after user click.
