fbc79a4f361cec52480e46f1bfbff41ddb7beb41
Mandy Mason <mnmason86@gmail.com>
2022-06-09 02:51:54 -0400

    continued working with render metho, and loop to call render method

--- app.js
+++ app.js
@@ -1,66 +1,79 @@
 'use-strict';
+//Global Variables
 
-//Array to hold product images
-
-OddDuckProducts.allProducts = [];
+let currentClicks = 0;
+let totalClicksAllowed = 25;
+let randomImages = [];
+let numberOfImages = 3;
 
 // Create a constructor function that creates an object associated with each product
 
-function OddDuckProducts(name,imgPath){
+function OddDuckProduct(name, imgPath){
   this.name = name;
   this.imgPath = imgPath;
   this.imgTimesShown = 0;
   this.imgTimesClicked = 0;
-  OddDuckProducts.allProducts.push(this);
 }
-
-new OddDuckProducts('baby-sweep', 'img/baby-sweep.png');
-new OddDuckProducts('banana', 'img/banana.jpg');
-new OddDuckProducts('bathroom', 'img/bathroom.jpg');
-new OddDuckProducts('boots', 'img/boots.jpg');
-new OddDuckProducts('breakfast', 'img/breakfast.jpg');
-new OddDuckProducts('bubblegum', 'img/bubblegum.jpg');
-new OddDuckProducts('chair', 'img/chair.jpg');
-new OddDuckProducts('cthulhu', 'img/cthulu.jpg');
-new OddDuckProducts('dog-duck', 'img/dog-duck.jpg');
-new OddDuckProducts('dragon', 'img/dragon,jpg');
-new OddDuckProducts('pen', 'img/pen.jpg');
-new OddDuckProducts('pet-sweep', 'img/pet-sweep.jpg');
-new OddDuckProducts('r2bag', 'img/r2bag.jpg');
-new OddDuckProducts('scissors', 'img/scissors.jpg');
-new OddDuckProducts('shark', 'img/shark.jpg');
-new OddDuckProducts('tauntaun', 'img/tauntaun.jpg');
-new OddDuckProducts('unicorn', 'img/unicorn.jpg');
-new OddDuckProducts('water-can', 'img/water-can.jpg');
-new OddDuckProducts('wine-glass', 'img/wine-glass.jpg');
+let allProducts = [
+  new OddDuckProduct('baby-sweep', 'img/baby-sweep.jpg'),
+  new OddDuckProduct('banana', 'img/banana.jpg'),
+  new OddDuckProduct('bathroom', 'img/bathroom.jpg'),
+  new OddDuckProduct('boots', 'img/boots.jpg'),
+  new OddDuckProduct('breakfast', 'img/breakfast.jpg'),
+  new OddDuckProduct('bubblegum', 'img/bubblegum.jpg'),
+  new OddDuckProduct('chair', 'img/chair.jpg'),
+  new OddDuckProduct('cthulhu', 'img/cthulu.jpg'),
+  new OddDuckProduct('dog-duck', 'img/dog-duck.jpg'),
+  new OddDuckProduct('dragon', 'img/dragon,jpg'),
+  new OddDuckProduct('pen', 'img/pen.jpg'),
+  new OddDuckProduct('pet-sweep', 'img/pet-sweep.jpg'),
+  new OddDuckProduct('r2bag', 'img/r2bag.jpg'),
+  new OddDuckProduct('scissors', 'img/scissors.jpg'),
+  new OddDuckProduct('shark', 'img/shark.jpg'),
+  new OddDuckProduct('tauntaun', 'img/tauntaun.jpg'),
+  new OddDuckProduct('unicorn', 'img/unicorn.jpg'),
+  new OddDuckProduct('water-can', 'img/water-can.jpg'),
+  new OddDuckProduct('wine-glass', 'img/wine-glass.jpg'),
+];
 
 // Create an algorithm that randomly generates 3 unique product images from the img directory and display them side-by-side in the browser window
 
 // Random number generator to select imgs from array
 
-let randomImages = [];
-
 function createRandomProducts(){
-  let randProduct = Math.floor(Math.random() * OddDuckProducts.allProducts.length);
-  randomImages.push(randProduct);
+  let randProduct = Math.floor(Math.random() * allProducts.length);
+  return allProducts[randProduct];
 }
-createRandomProducts();
-createRandomProducts();
-createRandomProducts();
+
+randomImages.push(createRandomProducts());
+randomImages.push(createRandomProducts());
+randomImages.push(createRandomProducts());
+
+
+console.log(randomImages);
+
+
+// Create render method for 1 image
+
+OddDuckProduct.prototype.render = function(){
+  let bodyContainer = document.getElementById('product-images');
+  let imgContainer = document.createElement('img');
+  imgContainer.src = `img/${this.name}.jpg`;
+  bodyContainer.appendChild(imgContainer);
+  imgContainer.width = 300;
+};
+
+//Loop through randomImages array to call render method
+
+for(let i = 0; i < randomImages.length; i++){
+  let randomImage = randomImages[i];
+  randomImage.render();
+}
+
 
 // Attach an Event Listener to the section of the HTML page where images are displayed.
 
 // Generate 3 new random products after user click.
 
 //Get image elements from the DOM and render
-
-OddDuckProducts.prototype.render = function(){
-  let bodyContainer = document.getElementById('product-images');
-  let imgContainer = document.createElement('img');
-  imgContainer.src = `img/${image.name}.jpg`;
-  bodyContainer.appendChild(imgContainer);
-  imgContainer.width = 300;
-};
-
-
 
