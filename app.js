e0ae8c1e926884002b1a9940ee2c6bda7662aed4
Mandy Mason <mnmason86@gmail.com>
2022-06-08 23:28:22 -0400

    appended images to page, updated baby-sweep file type from .png to .jpg

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ new OddDuckProducts('boots', 'img/boots.jpg');
 new OddDuckProducts('breakfast', 'img/breakfast.jpg');
 new OddDuckProducts('bubblegum', 'img/bubblegum.jpg');
 new OddDuckProducts('chair', 'img/chair.jpg');
-new OddDuckProducts('cthulu', 'img/cthulu.jpg');
+new OddDuckProducts('cthulhu', 'img/cthulu.jpg');
 new OddDuckProducts('dog-duck', 'img/dog-duck.jpg');
 new OddDuckProducts('dragon', 'img/dragon,jpg');
 new OddDuckProducts('pen', 'img/pen.jpg');
@@ -35,31 +35,42 @@ new OddDuckProducts('unicorn', 'img/unicorn.jpg');
 new OddDuckProducts('water-can', 'img/water-can.jpg');
 new OddDuckProducts('wine-glass', 'img/wine-glass.jpg');
 
-
-console.log(OddDuckProducts.allProducts);
-
 // Create an algorithm that randomly generates 3 unique product images from the img directory and display them side-by-side in the browser window
-
-//Get image elements from the DOM
-
-let product1Img = document.getElementById('product1');
-let product2Img = document.getElementById('product2');
-let product3Img = document.getElementById('product3');
 
 // Random number generator to select imgs from array
 
 function createRandomProduct(){
-  let randProduct1 = Math.floor(Math.random() * OddDuckProducts.allProducts.length);
-  let randProduct2 = Math.floor(Math.random()* OddDuckProducts.allProducts.length);
-  let randProduct3 = Math.floor(Math.random() * OddDuckProducts.allProducts.length);
-
-  console.log(randProduct1, randProduct2, randProduct3);
+  let randProduct = Math.floor(Math.random() * OddDuckProducts.allProducts.length);
+  return OddDuckProducts.allProducts[randProduct];
 }
 
-createRandomProduct();
+let image1 = createRandomProduct();
+let image2 = createRandomProduct();
+let image3 = createRandomProduct();
+
+//Get image elements from the DOM
+
+let bodyContainer = document.getElementById('product1');
+let imgContainer = document.createElement('img');
+imgContainer.src = `img/${image1.name}.jpg`, `img/${image2.name}.jpg`, `img/${image3.name}.jpg`;
+bodyContainer.appendChild(imgContainer);
+
+let bodyContainer2 = document.getElementById('product2');
+let imgContainer2 = document.createElement('img');
+imgContainer2.src =`img/${image2.name}.jpg`;
+bodyContainer2.appendChild(imgContainer2);
+
+let bodyContainer3 = document.getElementById('product3');
+let imgContainer3 = document.createElement('img');
+imgContainer3.src =`img/${image3.name}.jpg`;
+bodyContainer3.appendChild(imgContainer3);
+
+console.log(image1,image2,image3);
 
 // Attach an Event Listener to the section of the HTML page where images are displayed.
 
 
 
 // Generate 3 new random products after user click.
+
+
